4707139e7b206993ac65550de3be8e713aca7d8f
Namoah1 <namoah@macalester.edu>
2021-07-04 22:48:48 -0500

    Initial Commit

---
+++ Bin2Deci/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bin2Deci
+//
+//  Created by Nana Adwoa Odeibea Amoah on 7/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
