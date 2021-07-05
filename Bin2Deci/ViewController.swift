b9f637c66aff8323536b8eb4666f6a85ed1b70da
Namoah1 <namoah@macalester.edu>
2021-07-04 23:59:46 -0500

    Checks that input length is not greater than 8

--- Bin2Deci/ViewController.swift
+++ Bin2Deci/ViewController.swift
@@ -7,12 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var convertedLabel: UILabel!
     
     var result = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        inputTextField.delegate = self
+        
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let maxLength = 8
+        let currString: NSString = (textField.text ?? "") as NSString
+        let newString: NSString = currString.replacingCharacters(in: range, with: string) as NSString
+
+        return newString.length <= maxLength
+    }
     
     @IBAction func convertButton(_ sender: Any) {
         var inputString = inputTextField.text ?? ""
@@ -40,10 +54,7 @@ class ViewController: UIViewController {
     }
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+
 
 
 }
